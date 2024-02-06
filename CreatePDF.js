7b35c1917248c2d4eb2b33bbe01b552f930780e6
GCMoony <47188474+GCMoony@users.noreply.github.com>
2024-02-06 12:38:42 -0700

    Enumerate through all files

--- CreatePDF.js
+++ CreatePDF.js
@@ -3,30 +3,28 @@ const PDFDoc = require('pdfkit');
 const fs = require('node:fs');
 
 // Creating the PDF file and writing to system
-
 // Standard printer paper size
 const doc = new PDFDoc({size: 'A4'});
 doc.pipe(fs.createWriteStream("./myFile.pdf"));
 
+// Create array of files located in specified folder
+let files = fs.readdirSync("./assets")
+
+// Init page counter
+let pageCount = 1;
+
+
 // Adding each file to the PDF
-// Each image in its own page
-for(let i = 1; i < 19; i++ ) {
+// Each image on its own page
+for(let file of files) {
+  doc.image(`./assets/${file}`,0, 0, {width: 595.28, height: 841.89});
 
-    // Based on file name convention for now...
-    if(i < 10) {
-        doc.image(`./assets/0${i}.jpg`,0, 0, {width: 595.28, height: 841.89});
-    }
-     if(i > 9) {
-        doc.image(`./assets/${i}.jpg`,0, 0, {width: 595.28, height: 841.89});
-    }
-
-
-    if(i < 18) {
-        doc.addPage();
-    }
-    
+  // Add a new PDF page for the next image
+  if(pageCount <= files.length - 1) {
+    doc.addPage();
+  }
+  pageCount++;
 }
-
 
 // Close PDF
 doc.end();
